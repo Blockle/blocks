9423d4d36ad7f69fc7c4137106f5d3569dd8c92c
Niek Saarberg <niek@Nieks-MacBook-Air.fritz.box>
2023-10-07 21:56:57 +0200

    Updated exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 export * from './components';
 export { atoms } from './lib/css/atoms/atoms';
+export { breakpointQuery } from './lib/css/atoms/breakpoints';
+export { type ComponentThemesMap } from './lib/css/theme/componentThemes';
+export { makeTheme } from './lib/css/theme/makeTheme';
+export { type BlocksTokens } from './lib/css/theme/tokenType';
 export { vars } from './lib/css/theme/vars.css';
 export { classnames } from './lib/utils/classnames';
