34afd9ab1b762fa429d0f56566327a313ad821c3
Niek Saarberg <niek@Nieks-MacBook-Air.fritz.box>
2023-10-07 22:02:28 +0200

    Added another missing export

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { atoms } from './lib/css/atoms/atoms';
 export { breakpointQuery } from './lib/css/atoms/breakpoints';
 export { type ComponentThemesMap } from './lib/css/theme/componentThemes';
 export { makeTheme } from './lib/css/theme/makeTheme';
+export { makeComponentTheme } from './lib/css/theme/makeComponentTheme';
 export { type BlocksTokens } from './lib/css/theme/tokenType';
 export { vars } from './lib/css/theme/vars.css';
 export { classnames } from './lib/utils/classnames';
