3f4a5de4cd9010e61d21fbcd45e1fbfa290f1f55
nsaarberg <nsaarberg@xebia.com>
2023-10-07 20:56:58 +0200

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from './components';
 export { atoms } from './lib/css/atoms/atoms';
 export { vars } from './lib/css/theme/vars.css';
-export { setTheme } from './lib/theme/theme';
 export { classnames } from './lib/utils/classnames';
