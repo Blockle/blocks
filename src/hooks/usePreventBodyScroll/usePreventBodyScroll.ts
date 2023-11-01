beedb99f626f0bebf762dc9105f9ad37aa9eeb16
nsaarberg <nsaarberg@xebia.com>
2023-11-01 15:11:52 +0100

    usePreventBodyScroll fixes - Only update the body styles when enabled previously was true. - When there is a scrollbar visible set body to fixed and force a scrollbar on the y-axis

--- src/hooks/usePreventBodyScroll/usePreventBodyScroll.ts
+++ src/hooks/usePreventBodyScroll/usePreventBodyScroll.ts
@@ -2,14 +2,29 @@ import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect';
 
 export const usePreventBodyScroll = (enabled = true) => {
   useIsomorphicLayoutEffect(() => {
-    const prevValue = document.body.style.getPropertyValue('overflow');
+    if (!enabled) {
+      return;
+    }
 
-    if (enabled) {
+    const prevOverflow = document.body.style.getPropertyValue('overflow');
+
+    // When there is a scrollbar, body height is greater than window height
+    // Then we need to set body position to fixed to prevent body scroll
+    // and show scrollbar
+    // Otherwise, we set overflow to hidden to prevent body scroll
+    if (document.body.scrollHeight > window.innerHeight) {
+      document.body.style.position = 'fixed';
+      document.body.style.overflowY = 'scroll';
+    } else {
       document.body.style.overflow = 'hidden';
     }
 
+    // Revert back to previous overflow state when unmounting
+    // or when enabled is set to false
     return () => {
-      document.body.style.overflow = prevValue;
+      document.body.style.position = '';
+      document.body.style.overflow = prevOverflow;
+      document.body.style.overflowY = '';
     };
   }, [enabled]);
 };
