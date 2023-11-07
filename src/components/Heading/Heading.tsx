c0845ee0283e98404d7bd3480b7999163cb3f36d
Niek Saarberg <n.saarberg@gmail.com>
2023-11-07 14:13:08 +0100

    Removed "align" prop in Heading. Use "textAlign" instead

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,11 +1,10 @@
-import { Atoms, MarginAtoms, PaddingAtoms, TextAtoms } from '../../lib/css/atoms';
+import { MarginAtoms, PaddingAtoms, TextAtoms } from '../../lib/css/atoms';
 import { classnames } from '../../lib/utils/classnames';
 import { HTMLElementProps } from '../../lib/utils/utils';
 import { Box } from '../Box';
 import * as styles from './heading.css';
 
 export type HeadingProps = {
-  align?: Atoms['textAlign'];
   children: React.ReactNode;
   className?: string;
   level: 1 | 2 | 3 | 4 | 5 | 6;
