d0ad5b407fbe288d8111925f33c417c260979d82
Niek Saarberg <n.saarberg@gmail.com>
2023-11-09 18:27:49 +0100

    style function comments / cleanup

--- src/lib/css/style/style.ts
+++ src/lib/css/style/style.ts
@@ -1,13 +1,15 @@
 import { StyleRule, style as vanillaExtractStyle } from '@vanilla-extract/css';
+import { AnyString } from '../../utils/helpers';
 import { Atoms, atoms } from '../atoms';
 
 type StyleRuleWithAtoms = Omit<StyleRule, keyof Atoms> & {
-  [K in keyof Atoms]?: K extends keyof StyleRule
-    ? // eslint-disable-next-line @typescript-eslint/ban-types
-      Atoms[K] | (string & {}) | number // Could type it as: Atoms[K] | StyleRule[K]
-    : Atoms[K];
+  [K in keyof Atoms]?: K extends keyof StyleRule ? Atoms[K] | AnyString | number : Atoms[K];
 };
 
+/**
+ * A wrapper around vanilla-extract's `style` function that allows you to use
+ * atoms and raw CSS values in the same object.
+ */
 export function style(props: StyleRuleWithAtoms | (StyleRuleWithAtoms | string)[]): string {
   const styleRule: Record<string, unknown> = {};
   const atomClassNames: string[] = [];
@@ -27,7 +29,7 @@ export function style(props: StyleRuleWithAtoms | (StyleRuleWithAtoms | string)[
   for (const [name, value] of Object.entries(props)) {
     if ((atoms.properties as Set<string>).has(name)) {
       // atoms function throws an error if the value is not valid
-      // So use try/catch to fallback to raw CSS values
+      // So use try/catch to fallback to raw CSS values when atoms throws
 
       try {
         atomClassNames.push(atoms({ [name]: value }));
