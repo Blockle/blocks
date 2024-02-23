95657328933cd5bcf7b4548c36d50bc5971307d3
Niek Saarberg <n.saarberg@gmail.com>
2024-02-23 14:55:02 +0100

    Added temporary icon to Select component

--- src/components/form/Select/Select.tsx
+++ src/components/form/Select/Select.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react';
+import React, { forwardRef } from 'react';
 import { useComponentStyles } from '../../../hooks/useComponentStyles';
 import { SelectTheme } from '../../../lib/theme/componentThemes';
 import { classnames } from '../../../lib/utils/classnames';
@@ -31,8 +31,22 @@ export const Select = forwardRef<HTMLSelectElement, SelectProps>(function Select
         {children}
       </select>
       <Box className={classnames(styles.icon, iconClassName)} role="presentation" aria-hidden>
-        ICON
+        <DefaultIcon />
       </Box>
     </Box>
   );
 });
+
+const DefaultIcon: React.FC = () => {
+  return (
+    // TOOD - replace with actual icon component renderer
+    <svg viewBox="0 0 24 24" style={{ width: '1rem', height: '1rem' }}>
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="m11.625 14.3666 7.9365-7.653c.5241-.5055 1.3133-.4372 1.7625.1525.4493.5897.3886 1.4774-.1355 1.9829l-8.75 8.4375c-.4681.4513-1.1589.4513-1.627 0l-8.75-8.4375c-.5241-.5055-.5848-1.3932-.1356-1.9829.4493-.5897 1.2385-.658 1.7626-.1525l7.9365 7.653Z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
