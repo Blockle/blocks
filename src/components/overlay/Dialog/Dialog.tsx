b53d65c148e3972a2bfc06ee51cccb5e02baefc1
Niek Saarberg <n.saarberg@gmail.com>
2024-06-12 22:34:09 +0200

    Playing around with native dialog

--- src/components/overlay/Dialog/Dialog.tsx
+++ src/components/overlay/Dialog/Dialog.tsx
@@ -2,14 +2,11 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 import { useComponentStyles } from '../../../hooks/useComponentStyles';
 import { useIsomorphicLayoutEffect } from '../../../hooks/useIsomorphicLayoutEffect';
 import { useKeyboard } from '../../../hooks/useKeyboard';
-import { useLayer } from '../../../hooks/useLayer';
 import { usePreventBodyScroll } from '../../../hooks/usePreventBodyScroll';
 import { useRestoreFocus } from '../../../hooks/useRestoreFocus';
-import { useVisibilityState } from '../../../hooks/useVisibilityState';
 import { DialogTheme } from '../../../lib/theme/componentThemes';
 import { classnames } from '../../../lib/utils/classnames';
 import { hasAnimationDuration } from '../../../lib/utils/dom';
-import { Portal } from '../../other/Portal';
 import * as styles from './dialog.css';
 import { DialogContext, useNestedDialog } from './dialogHelper';
 
@@ -32,19 +29,18 @@ export const Dialog: React.FC<DialogProps> = ({
 }) => {
   const dialogClassName = useComponentStyles('dialog', { dialog: true, variants: { size } });
   const dialogRef = useRef<HTMLDialogElement>(null);
-  const layer = useLayer();
-  const [visible, hide] = useVisibilityState(open);
   const [enabled, setEnabled] = useState(true);
+  const [visible, setVisible] = useState(open);
   const lastAction = useRef(0);
 
   // Store the previous active element and restore it when the dialog is closed
   useRestoreFocus(open);
 
   // Disable functionality of parent dialogs and return boolean if this dialog is nested
-  const isNested = useNestedDialog(visible);
+  const isNested = useNestedDialog(open);
 
   // Diable hook for nested dialogs, top level dialog already handles this
-  usePreventBodyScroll(visible && !isNested);
+  usePreventBodyScroll(open && !isNested);
 
   const onEscape = useCallback(
     (event: KeyboardEvent) => {
@@ -74,11 +70,9 @@ export const Dialog: React.FC<DialogProps> = ({
         return;
       }
 
-      if (dialogRef.current?.contains(event.target as Node)) {
-        return;
+      if (!dialogRef.current?.contains(event.target as Node)) {
+        onRequestClose();
       }
-
-      onRequestClose();
     };
 
     document.addEventListener('click', listener);
@@ -89,63 +83,43 @@ export const Dialog: React.FC<DialogProps> = ({
   }, [enabled, onRequestClose, open]);
 
   useIsomorphicLayoutEffect(() => {
-    if (!visible) {
+    // Using an addional state to control the visibility of the dialog
+    if (open && visible) {
+      dialogRef.current?.showModal();
+    } else if (open) {
+      setVisible(true);
+    } else {
+      // If the dialog has no animation duration, we need to manually hide it
+      if (!hasAnimationDuration(dialogRef.current)) {
+        setVisible(false);
+      }
+
       dialogRef.current?.close();
     }
-
-    if (!open) {
-      dialogRef.current?.removeAttribute('data-open');
-      return;
-    }
-
-    // Use a double requestAnimationFrame to ensure transitions are applied
-    let timer = requestAnimationFrame(() => {
-      timer = requestAnimationFrame(() => {
-        dialogRef.current?.showModal();
-        dialogRef.current?.setAttribute('data-open', '');
-      });
-    });
-
-    return () => {
-      cancelAnimationFrame(timer);
-    };
   }, [open, visible]);
 
-  // Hide the dialog when the animation ends
   const onAnimationEnd = useCallback(() => {
     if (!open) {
-      hide();
+      console.log('onAnimationEnd');
+      setVisible(false);
     }
-  }, [hide, open]);
-
-  // Hide the dialog immediately when the open prop changes to false
-  // and no animation is used
-  useEffect(() => {
-    if (open) {
-      return;
-    }
-
-    // If the dialog has no transition, hide it immediately
-    if (!hasAnimationDuration(dialogRef.current)) {
-      hide();
-    }
-  }, [hide, open]);
+  }, [setVisible, open]);
 
   if (!visible) {
     return null;
   }
 
   // SSR: If the dialog is open on the server, we need to render it with the open attribute
-  const dataOpen = typeof window === 'undefined' && open ? '' : undefined;
+  const dataOpen = typeof window === 'undefined' && open ? true : undefined;
 
   return (
-    <Portal container={layer()}>
+    <>
       {/* Portals render outside the "root", so make sure to wrap our dialog within the theme provider */}
       <DialogContext.Provider value={{ setEnabled }}>
         <dialog
           ref={dialogRef}
           aria-modal="true"
-          data-open={dataOpen}
+          open={dataOpen}
           className={classnames(styles.dialog, dialogClassName, className)}
           onAnimationEnd={onAnimationEnd}
           onTransitionEnd={onAnimationEnd}
@@ -154,6 +128,6 @@ export const Dialog: React.FC<DialogProps> = ({
           {children}
         </dialog>
       </DialogContext.Provider>
-    </Portal>
+    </>
   );
 };
