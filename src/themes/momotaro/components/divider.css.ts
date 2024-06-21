82c24acdcc704866a348a9ffbfdf310c4ffaaaaa
nsaarberg <nsaarberg@xebia.com>
2024-06-21 11:38:07 +0200

    Changed width to inlineSize in Divider

--- src/themes/momotaro/components/divider.css.ts
+++ src/themes/momotaro/components/divider.css.ts
@@ -1,6 +1,10 @@
+import { style } from '@vanilla-extract/css';
 import { makeComponentTheme } from '../../../lib/theme/makeComponentTheme';
 
 export const divider = makeComponentTheme('divider', {
+  base: style({
+    blockSize: 1,
+  }),
   defaultVariants: {
     color: 'textLight',
   },
