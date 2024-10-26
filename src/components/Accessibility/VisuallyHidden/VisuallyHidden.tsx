7c1cdf678327b238f971ddfaeaec003f1bc1d5b5
Niek Saarberg <n.saarberg@gmail.com>
2024-10-26 20:38:42 +0200

    Added html props spread to VisuallyHidden

--- src/components/Accessibility/VisuallyHidden/VisuallyHidden.tsx
+++ src/components/Accessibility/VisuallyHidden/VisuallyHidden.tsx
@@ -1,9 +1,19 @@
+import { classnames } from '../../../lib/utils/classnames';
+import { HTMLElementProps } from '../../../lib/utils/utils';
 import * as styles from './visually-hidden.css';
 
 export type VisuallyHiddenProps = {
   children: React.ReactNode;
-};
+} & HTMLElementProps<HTMLSpanElement>;
 
-export const VisuallyHidden: React.FC<VisuallyHiddenProps> = ({ children }) => {
-  return <span className={styles.visuallyHidden}>{children}</span>;
+export const VisuallyHidden: React.FC<VisuallyHiddenProps> = ({
+  children,
+  className,
+  ...restProps
+}) => {
+  return (
+    <span className={classnames(styles.visuallyHidden, className)} {...restProps}>
+      {children}
+    </span>
+  );
 };
