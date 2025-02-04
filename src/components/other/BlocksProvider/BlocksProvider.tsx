d1c2068b74dbf6baa5ed9a95602ee5a44dce3157
Niek Saarberg <n.saarberg@gmail.com>
2025-02-04 16:15:25 +0100

    React 19 supports inert, so removed legacy code

--- src/components/other/BlocksProvider/BlocksProvider.tsx
+++ src/components/other/BlocksProvider/BlocksProvider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { atoms } from '../../../lib/css/atoms/sprinkles.css';
 import { Theme } from '../../../lib/theme/makeTheme';
 import { classnames } from '../../../lib/utils/classnames';
@@ -17,7 +17,6 @@ export const BlocksProvider: React.FC<BlocksProviderProps> = ({
   className,
   ...restProps
 }) => {
-  const ref = useRef<HTMLDivElement>(null);
   const [ariaHidden, setAriaHidden] = useState(false);
   const contextValue = useMemo(
     () => ({
@@ -27,28 +26,15 @@ export const BlocksProvider: React.FC<BlocksProviderProps> = ({
     [theme, setAriaHidden],
   );
 
-  // This is a workaround for React's lack of inert attribute support
-  // https://github.com/facebook/react/issues/17157
-  useEffect(() => {
-    if (ariaHidden) {
-      ref.current?.setAttribute('inert', '');
-    } else {
-      ref.current?.removeAttribute('inert');
-    }
-  });
-
   return (
-    <BlocksProviderContext.Provider value={contextValue}>
+    <BlocksProviderContext value={contextValue}>
       <div
-        ref={ref}
         className={classnames(theme.vars, atoms({ fontFamily: 'primary' }), className)}
-        aria-hidden={ariaHidden ? true : undefined}
-        // TODO Remove ref and useEffect when React supports inert attribute
-        // inert={ariaHidden}
+        inert={ariaHidden ? true : undefined}
         {...restProps}
       >
         {children}
       </div>
-    </BlocksProviderContext.Provider>
+    </BlocksProviderContext>
   );
 };
