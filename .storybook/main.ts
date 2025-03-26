50cabb43477521941396659c27e794de5ed2f029
Niek Saarberg <n.saarberg@gmail.com>
2025-03-26 21:35:51 +0100

    Added Divider Storybook upgrade

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,4 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from '@storybook/react-vite';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { mergeConfig } from 'vite';
@@ -14,13 +15,13 @@ const config: StorybookConfig = {
     '../packages/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y',
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-a11y"),
   ],
   framework: {
-    name: '@storybook/react-vite',
+    name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
   docs: {},
@@ -29,3 +30,7 @@ const config: StorybookConfig = {
   },
 };
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
