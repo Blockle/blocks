c60a62961bd57dd097a0ff451ef48979eef0cf87
nsaarberg <nsaarberg@xebia.com>
2025-10-01 20:41:01 +0200

    Using layers for the css reset

--- packages/reset/src/reset.css.ts
+++ packages/reset/src/reset.css.ts
@@ -1,46 +1,79 @@
+import { blocksLayerAtom } from '@blockle/blocks';
 import { globalStyle } from '@vanilla-extract/css';
 
 // Must be placed before HTML so we wont override the box-sizing of the root element
 globalStyle(':where(*, *::before, *::after)', {
-  boxSizing: 'inherit',
-  WebkitTapHighlightColor: 'transparent',
+  '@layer': {
+    [blocksLayerAtom]: {
+      boxSizing: 'inherit',
+      WebkitTapHighlightColor: 'transparent',
+    },
+  },
 });
 
 globalStyle(':where(html)', {
-  lineHeight: 1.5,
-  boxSizing: 'border-box',
-  WebkitFontSmoothing: 'antialiased',
+  '@layer': {
+    [blocksLayerAtom]: {
+      lineHeight: 1.5,
+      boxSizing: 'border-box',
+      WebkitFontSmoothing: 'antialiased',
+    },
+  },
 });
 
 globalStyle(':where(body)', {
-  margin: 0,
-  padding: 0,
-  fontFamily: 'Calibri, sans-serif',
+  '@layer': {
+    [blocksLayerAtom]: {
+      margin: 0,
+      padding: 0,
+      fontFamily: 'Calibri, sans-serif',
+    },
+  },
 });
 
 globalStyle(':where(button, input, optgroup, select, textarea)', {
-  fontFamily: 'inherit',
-  fontSize: '100%',
-  lineHeight: 'inherit',
-  margin: 0,
-  padding: 0,
+  '@layer': {
+    [blocksLayerAtom]: {
+      fontFamily: 'inherit',
+      fontSize: '100%',
+      lineHeight: 'inherit',
+      margin: 0,
+      padding: 0,
+    },
+  },
 });
 
 globalStyle(':where(p, ul, ol, pre, blockquote)', {
-  margin: 0,
-  padding: 0,
+  '@layer': {
+    [blocksLayerAtom]: {
+      margin: 0,
+      padding: 0,
+    },
+  },
 });
 
 globalStyle(':where(h1, h2, h3, h4, h5, h6)', {
-  margin: 0,
-  padding: 0,
-  fontSize: 'inherit',
+  '@layer': {
+    [blocksLayerAtom]: {
+      margin: 0,
+      padding: 0,
+      fontSize: 'inherit',
+    },
+  },
 });
 
 globalStyle(':where(pre)', {
-  whiteSpace: 'pre-wrap',
+  '@layer': {
+    [blocksLayerAtom]: {
+      whiteSpace: 'pre-wrap',
+    },
+  },
 });
 
 globalStyle(':where([popover])', {
-  border: 'unset',
+  '@layer': {
+    [blocksLayerAtom]: {
+      border: 'unset',
+    },
+  },
 });
