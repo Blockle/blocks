f524723eb6bc119c16bc5e3ff700ddbc646cc55a
Niek Saarberg <n.saarberg@gmail.com>
2025-11-15 01:17:32 +0100

    Re-added lint exception

--- packages/react/src/hooks/useControlledValue/useControlledValue.ts
+++ packages/react/src/hooks/useControlledValue/useControlledValue.ts
@@ -44,6 +44,7 @@ export function useControlledValue<T>({
 
   // Warn if the component is in controlled mode but no value is provided
   if (!import.meta.env.PROD) {
+    // biome-ignore lint/correctness/useHookAtTopLevel: is a dev-only hook
     useEffect(() => {
       if (onChange && value === undefined) {
         console.error('Slider is in controlled mode but no value is provided');
