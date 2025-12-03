294f9e36d6076bfe8c0fdbc297496cf5e0fcabdd
nsaarberg <nsaarberg@xebia.com>
2025-12-03 18:33:38 +0100

    Code formatting

--- packages/react/src/components/display/Icon/createIconMask.tsx
+++ packages/react/src/components/display/Icon/createIconMask.tsx
@@ -1,11 +1,11 @@
-import { IconMask, type IconMaskProps } from "./IconMask.js";
+import { IconMask, type IconMaskProps } from './IconMask.js';
 
-type CreateIconMaskProps<TName extends string> = Omit<IconMaskProps, "src"> & {
+type CreateIconMaskProps<TName extends string> = Omit<IconMaskProps, 'src'> & {
   name: TName;
 };
 
 export const createIconMask = <TName extends string>(
-  getIconUrl: (name: TName) => string
+  getIconUrl: (name: TName) => string,
 ) => {
   const CustomIconMask = ({ name, ...props }: CreateIconMaskProps<TName>) => {
     return <IconMask {...props} src={getIconUrl(name)} />;
