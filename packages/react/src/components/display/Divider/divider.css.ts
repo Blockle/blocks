147cf5575bf9bc6dbcbd2795dadd79b41eece362
Niek Saarberg <n.saarberg@gmail.com>
2026-01-21 23:07:34 +0100

    Enhance Divider component with size variants and improved styling

--- packages/react/src/components/display/Divider/divider.css.ts
+++ packages/react/src/components/display/Divider/divider.css.ts
@@ -1,11 +1,40 @@
 import { layers } from '@blockle/blocks-core';
-import { style } from '@vanilla-extract/css';
+import { createVar, style } from '@vanilla-extract/css';
+
+export const dividerColorVar = createVar();
 
 export const divider = style({
   '@layer': {
     [layers.molecule]: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      textAlign: 'right',
       clear: 'both',
-      blockSize: '1px',
+      minBlockSize: '1px',
+      ':empty': {
+        gap: 0,
+      },
+      ':before': {
+        flex: 1,
+        content: '""',
+        display: 'block',
+        backgroundColor: dividerColorVar,
+      },
+      ':after': {
+        flex: 1,
+        content: '""',
+        display: 'block',
+        backgroundColor: dividerColorVar,
+      },
+      selectors: {
+        '&[data-align="start"]:before': {
+          maxInlineSize: '3rem',
+        },
+        '&[data-align="end"]:after': {
+          maxInlineSize: '3rem',
+        },
+      },
     },
   },
 });
