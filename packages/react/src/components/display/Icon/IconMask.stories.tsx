222e443a0cce8789057d1e606206816b148957cb
Niek Saarberg <n.saarberg@gmail.com>
2026-01-22 21:36:41 +0100

    Corrected IconMask story

--- packages/react/src/components/display/Icon/IconMask.stories.tsx
+++ packages/react/src/components/display/Icon/IconMask.stories.tsx
@@ -1,44 +1,23 @@
-import { atomicProperties } from '@blockle/blocks-core';
-
 import preview from '../../../../../../.storybook/preview.js';
 import type { HeroIconName } from './heroicons.js';
 import { IconMask } from './IconMask.js';
-
-const meta = preview.meta({
-  title: 'Display/IconMask',
-  component: IconMask,
-  argTypes: {
-    color: {
-      control: 'select',
-      options: Object.keys(atomicProperties.color.values),
-    },
-  },
-});
 
 function getIconUrl(name: HeroIconName): string {
   return `/heroicons/outline/${name}.svg`;
 }
 
+const meta = preview.meta({
+  title: 'Components/Icon/IconMask',
+  component: IconMask,
+});
+
 export const Default = meta.story({
   args: {
     src: getIconUrl('academic-cap'),
   },
+  argTypes: {
+    src: {
+      control: 'text',
+    },
+  },
 });
-
-// TODO Fix me
-// export const HeroIcons = meta.story({
-//   render(props) {
-//     return <HeroIcon {...props} />;
-//   },
-//   args: {
-//     name: 'academic-cap',
-//   },
-//   argTypes: {
-//     name: {
-//       control: 'select',
-//       options: heroIconNames,
-//     },
-//     // @ts-expect-error src not in typing, but exists in runtime
-//     src: { table: { disable: true } },
-//   },
-// });
