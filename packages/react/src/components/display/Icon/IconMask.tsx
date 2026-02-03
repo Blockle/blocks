d552b1c7aecf92bd13d029343426cbadbf43498b
Niek Saarberg <n.saarberg@gmail.com>
2026-02-03 19:40:36 +0100

    Allow more atoms to be passed to "IconMask" component.

--- packages/react/src/components/display/Icon/IconMask.tsx
+++ packages/react/src/components/display/Icon/IconMask.tsx
@@ -3,6 +3,8 @@ import {
   atoms,
   type ComponentThemes,
   classnames,
+  getAtomsAndProps,
+  type MarginAtoms,
 } from '@blockle/blocks-core';
 import type React from 'react';
 
@@ -14,16 +16,21 @@ type IconTheme = ComponentThemes['icon'];
 export type IconMaskProps = {
   className?: string;
   color?: Atoms['color'];
-  src: string;
+  display?: Atoms['display'];
   size?: IconTheme['variants']['size'];
-};
+  src: string;
+  style?: React.CSSProperties;
+} & MarginAtoms;
 
 export const IconMask: React.FC<IconMaskProps> = ({
   className,
   color = 'currentColor',
   src,
   size,
+  style,
+  ...restProps
 }) => {
+  const [atomsProps, otherProps] = getAtomsAndProps(restProps);
   const iconClassName = useComponentStyles('icon', {
     variants: { size },
   });
@@ -32,14 +39,16 @@ export const IconMask: React.FC<IconMaskProps> = ({
     <span
       role="presentation"
       className={classnames(
-        atoms({ color }),
+        atoms({ color, ...atomsProps }),
         styles.iconMask,
         iconClassName,
         className,
       )}
       style={{
         mask: `url(${src}) no-repeat center`,
+        ...style,
       }}
+      {...otherProps}
     />
   );
 };
