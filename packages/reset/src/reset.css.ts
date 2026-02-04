b2198704a1d770a6830690bdabf5e7ee319dbf5c
Niek Saarberg <n.saarberg@gmail.com>
2026-02-04 15:54:53 +0100

    Corrected layers in reset css

--- packages/reset/src/reset.css.ts
+++ packages/reset/src/reset.css.ts
@@ -2,11 +2,13 @@ import { layers } from '@blockle/blocks-core';
 import { globalStyle } from '@vanilla-extract/css';
 
 globalStyle(':where(*,:after,:before,::backdrop)', {
-  [layers.reset]: {
-    boxSizing: 'inherit',
-    border: '0 none',
-    margin: 0,
-    padding: 0,
+  '@layer': {
+    [layers.reset]: {
+      boxSizing: 'inherit',
+      border: '0 none',
+      margin: 0,
+      padding: 0,
+    },
   },
 });
 
@@ -41,8 +43,12 @@ globalStyle(':where(pre)', {
 });
 
 globalStyle(':where(h1,h2,h3,h4,h5,h6)', {
-  fontSize: 'inherit',
-  fontWeight: 'inherit',
+  '@layer': {
+    [layers.reset]: {
+      fontSize: 'inherit',
+      fontWeight: 'inherit',
+    },
+  },
 });
 
 globalStyle(':where(a)', {
